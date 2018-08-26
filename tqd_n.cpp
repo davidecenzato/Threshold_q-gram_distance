6a350ff71a302f58f98eb00f845212e2591265cd
Alessio Milanese <milanese.alessio@gmail.com>
2018-08-26 09:20:23 +0200

    input is provided by program arguments and add verbose

--- tqd_n.cpp
+++ tqd_n.cpp
@@ -6,7 +6,7 @@
 #include "Profile.cpp"
 
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
@@ -34,9 +34,9 @@ char NucleicAlphabet [4] = {'a','t','c','g'};
 char NucleicAlphabetUpperCase [4] = {'A','T','C','G'};
 
 
-//----------------------------------------------------------------------------------//
-//                               FUNCTIONS DECLARATION                              //
-//----------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------//
+//                               FUNCTIONS DECLARATION                        //
+//----------------------------------------------------------------------------//
 // convert from string to an array of numbers
 // (we doesn't work with string, but with arrays of integer)
 void str2num (int num[], int NumDim, char str[]);
@@ -44,11 +44,11 @@ void str2num (int num[], int NumDim, char str[]);
 //load a fasta file
 char* load_fasta(char file_name[]);
 
-// calculate the profiles of two strings and save it in a RB-tree 
+// calculate the profiles of two strings and save it in a RB-tree
 void createProfile (int str1[], int strlen1, int str2[], int strlen2);
 
-// calculate the position in the profile of a given q-gram, and calculate the q-gram
-// given a position in the profile
+// calculate the position in the profile of a given q-gram, and calculate the
+// q-gram given a position in the profile
 unsigned long long int qposition (int vec[],int size_vec);
 void qgramFromPosition (int vec[], unsigned long long int pos);
 
@@ -59,105 +59,77 @@ unsigned long long int myPow(int a ,int b);
 
 
 
-//----------------------------------------------------------------------------------//
-//                                         MAIN                                     //
-//----------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------//
+//                                   MAIN                                     //
+//----------------------------------------------------------------------------//
 int main(int argc, char **argv){
-	
-	//------------------------------------------------------------------------
-	//INIZIALIZING THE VARIABLES
-	//------------------------------------------------------------------------
+
+  //----------------------------------------------------------------------------
+  // INPUT ---------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+  // q
+  q = std::stoi (argv[1]);
+  // threshold
+  int threshold_val = std::stoi (argv[2]);
+  // type_input: 1 strings, 2 files
+  int type_input = std::stoi (argv[3]);
+  // input 1
+  char *input1 = argv[4];
+  // input 2
+  char *input2 = argv[5];
+  // verbose level
+  int verbose = std::stoi (argv[6]);
+
+  //check the value for q
+	if (q > 32){
+    printf("Error. Maximum value for q is 32.\n");
+    exit(1);
+	}
+
+	//----------------------------------------------------------------------------
+	// INIZIALIZING THE VARIABLES
+	//----------------------------------------------------------------------------
+  if (verbose > 3){printf("Inizialise the variables \n");}
 	char *str1;
 	char *str2;
 
-	
-	printf("-----------------------------------------\n");
-
-	printf("size of the alphabet: %d\n",alphSize);
-	printf("alphabet: a, t, c, g, A, T, C, G.");
-	printf("\n\n");
-	
-	
-	//inizializing the variables
-	printf ("Insert q: ");	
-	scanf ("%d",&q);
-	
 	//create a variable to calculate the time to load the fasta files
-	time_t m1;
-   
-	
-	
-	 //control the maximum number we could use for q
-	 if (q > 32){
-	   printf("Error. You exceed the size of int to represent a q-gram (max q is 32).\n");
-	   exit(1);
-	 }
-	
-	//create the inputs strings
-	char strfile1[30]; //file name 1
-	char strfile2[30]; //file name 2
-	printf("Do you want to insert strings (1) or file (2)? ");
-	int ris=2;
-	scanf ("%d",&ris);
-	if (ris != 2){ //insert strings manually
-		str1 = (char *)malloc(max_string_length*sizeof(char));
-		str2 = (char *)malloc(max_string_length*sizeof(char));
-		if (str1 == NULL || str2 == NULL){
-			printf("\n Error.\n Out of memory.\n");
-		}
-		printf ("Insert string 1: ");	
-		scanf ("%s",str1);
-		printf ("Insert string 2: ");	
-		scanf ("%s",str2);
-	}else{  //insert strings from a fasta file
-		printf ("Insert file 1: ");	
-		scanf ("%s",strfile1);
-		printf ("Insert file 2: ");	
-		scanf ("%s",strfile2);
-		printf("\nLoad first file... ");
-		
-		//inizializing the time before loading the files
-		time_t now1 = time(NULL);
-		
-    	str1 = load_fasta(strfile1);
-    	printf("done.\n");
-    	printf("Load second file... ");
-    	str2 = load_fasta(strfile2);
-    	printf("done.\n\n");
-    	
-    	//calculate the time to load the fasta file
-    	m1 = difftime(time(NULL), now1);
-    }
-    
-    //------------------------------------------------------------------------
-    //BEGINNING OF THE COMPUTATION
-    //------------------------------------------------------------------------
-    
-   //inizializing the variables to calculate the time for the computation
-   time_t m;
-   time_t now = time(NULL);
-   
+  time_t now1 = time(NULL);
 
-    //calculate the length of the strings 
-    printf("Calculate the length of the strings... ");
-    int len1 = strlen(str1);
-    int len2 = strlen(str2);
-    if (len1<q){
-          printf("Error. the first string is shorter then q.\n");
-          exit(1);
-    }
-    if (len2<q){
-          printf("Error. the second string is shorter then q.\n");
-          exit(1);
-    }
-    
-    printf("done.\n");
-    printf("First string:  %d\n",len1);
-    printf("Second string: %d\n\n",len2);
-    
+  if (type_input == 1){  // the input are strings
+    str1 = argv[4];
+    str2 = argv[5];
+  }
 
-	//convert the strings in arrays of numbers
-	printf("Convert the strings in numbers... ");
+  if (type_input == 2){
+    str1 = load_fasta(input1);
+    str2 = load_fasta(input2);
+  }
+
+  time_t m1 = difftime(time(NULL), m1);
+
+
+  //----------------------------------------------------------------------------
+  //BEGINNING OF THE COMPUTATION
+  //----------------------------------------------------------------------------
+  //calculate the length of the strings ----------------------------------------
+  if (verbose > 3){printf("Calculate the length of the strings.\n");}
+  int len1 = strlen(str1);
+  int len2 = strlen(str2);
+  if (len1<q){
+        printf("Error. the first string is shorter then q.\n");
+        exit(1);
+  }
+  if (len2<q){
+        printf("Error. the second string is shorter then q.\n");
+        exit(1);
+  }
+  if (verbose > 3){printf("First string:  %d\n",len1);}
+  if (verbose > 3){printf("Second string: %d\n",len2);}
+
+
+	//convert the strings in arrays of numbers -----------------------------------
+	if (verbose > 3){printf("Convert the strings in numbers.\n");}
 	int *numericStr1;
 	int *numericStr2;
 	numericStr1 = (int *)malloc(len1*sizeof(int));
@@ -165,71 +137,61 @@ int main(int argc, char **argv){
 	if (numericStr1 == NULL || numericStr2 == NULL){
 		printf("\n Error.\n Out of memory.\n");
 	}
-	
+
 	str2num(numericStr1,len1,str1);
-	free(str1); // free memory of the char string
-
 	str2num(numericStr2,len2,str2);
-	free(str2); // free memory of the char string
-	
-	printf("done.\n\n");
 
-	
-	
+  if (type_input == 2){
+    free(str1); // free memory of the char string
+    free(str2); // free memory of the char string
+  }
+
 	//----------calculate the profile
-	
-	printf("\nCalculate the profile... ");  
-	createProfile (numericStr1, len1, numericStr2, len2);
-	
-	printf("done.\n\n");
-	
-	
-	//calculate the distance
-	printf("Calculate the distance..."); 
 
+	if (verbose > 3){printf("Calculate the profile\n");}
+	createProfile (numericStr1, len1, numericStr2, len2);
+
+	//calculate the distance
+	if (verbose > 3){printf("Calculate the distance\n");}
 	unsigned long long int dist = profile.calculateDistance();
-	
-	printf("done.\n\n");
-	
+
 	//calculate the total pair-status
-	printf("Calcolate the pair statuses..."); 
+	if (verbose > 3){printf("Calcolate the pair statuses\n");}
 
 	int *status;
 	status=profile.get_total_status();
-	
-	printf("done.\n\n");
-	
+
 	//------------------------------------------------------------------------
 	//PRINT THE RESULTS
 	//------------------------------------------------------------------------
 	printf("------------------------------------------\n");
 	printf("RESULTS\n");
 	printf("------------------------------------------\n");
-	
+
 	printf("=== INPUT: ===============================\n");
-	
+
 	printf("alphabet: a, t, c, g, A, T, C, G.\n");
 	cout << "q: "<< q <<"\n";
-	if(ris==2){
-		cout<< "first string: "<< strfile1 << "\n";
-		cout<< "second string: "<< strfile2 << "\n\n";
+	if(type_input==2){
+		cout<< "first string: "<< input1 << "\n";
+		cout<< "second string: "<< input2 << "\n\n";
 	}
-	
-	
+
+
 	printf("\n=== TIME: ================================\n");
-	
-	if(ris==2){
+
+	if(type_input==2){
 		printf("time to load the fasta files (seconds): %ld\n",m1);
 	}
-	m = difftime(time(NULL), now);
-    printf("calculate time (seconds): %ld\n\n",m);
-	
-	
+//	m = difftime(time(NULL), now);
+  //  printf("calculate time (seconds): %ld\n\n",m);
+
+
 	printf("\n=== THRESHOLD Q-GRAM DISTANCE: ===========\n");
-	
+
 	cout << "number of nodes: " << profile.getNumber_of_nodes()<<"\n";
 	//printf("percentage: %.2f\n\n",(float(dist)/n_nodes));
-	
+
 	cout<<"counting pair status in the profiles:\n";
 	for (int i=0;i<3;i++){
 		for (int j=0;j<3;j++){
@@ -239,9 +201,7 @@ int main(int argc, char **argv){
 	printf("\n");
 
 	cout << "\nTQD: "<<dist<<"\n";
-	
-	scanf ("%s",strfile1);
-	
+
 }
 
 
@@ -253,7 +213,7 @@ int main(int argc, char **argv){
 //----------------------------------------------------------------------------------//
 
 //////////////////////  LOAD FASTA  -------------------------------------------------------------
-// load a fasta file 
+// load a fasta file
 
 char* load_fasta(char file_name[]){
 	// we create the string str that will contain the string that we are going to read
@@ -261,11 +221,11 @@ char* load_fasta(char file_name[]){
 	if (str == NULL){
 		printf("\n Error.\n Out of memory.\n");
 	}
-	
+
 	// create and read the FILE
 	FILE *f;
 	f = fopen(file_name, "r");
-	
+
 	if (f){
 		//the first line is description
 		while(fgetc(f) != '\n'){
@@ -290,10 +250,10 @@ char* load_fasta(char file_name[]){
 }
 
 //////////////////////  STRING => ARRAY OF NUMBERS  -------------------------------------------------------------
-// convert a string in an array of number (according to the alphabet)  
+// convert a string in an array of number (according to the alphabet)
 void str2num (int num[], int NumDim, char str[]){
 	int correct=0;
-	
+
 	for(int i=0;i<NumDim;i++){
 		for (int j=0;j<alphSize;j++){
 			if (str[i]==NucleicAlphabet[j] || str[i]==NucleicAlphabetUpperCase[j] ){
@@ -306,21 +266,21 @@ void str2num (int num[], int NumDim, char str[]){
 			   exit(1);
 			}
 		}
-	}	
+	}
 }
 //////////////////////  CREATE PROFILE  --------------------------------------------------------------------------
-// calculate the profile of a given string    
+// calculate the profile of a given string
 
 void createProfile (int str1[], int strlen1, int str2[], int strlen2){
-	
+
 	unsigned long long int pos=0;
 	int qgram[q];
-	
+
 	// find the first q-gram of the FIRST string------------------------------
-	for (int j=0;j<q;j++){   
+	for (int j=0;j<q;j++){
 			qgram[j]=str1[j];
 	}
-	
+
 	// calculate the position of the first q-gram and update the profile
 	pos = qposition(qgram,q);
 	profile.insert(pos,1);
@@ -333,15 +293,15 @@ void createProfile (int str1[], int strlen1, int str2[], int strlen2){
 		//insert
 		profile.insert(pos,1);
 	}
-	
+
 	// calculate the profile for the second string
 	pos=0;
-	
+
 	// find the first q-gram of the SECOND string------------------------------
-	for (int j=0;j<q;j++){   
+	for (int j=0;j<q;j++){
 			qgram[j]=str2[j];
 	}
-	
+
 	// calculate the position of the first q-gram and update the profile
 	pos = qposition(qgram,q);
 	profile.insert(pos,2);
@@ -353,11 +313,11 @@ void createProfile (int str1[], int strlen1, int str2[], int strlen2){
 		//insert
 		profile.insert(pos,2);
 
-	}	
-    return;	
+	}
+    return;
 }
 //////////////////////  Q-GRAM => PROFILE POSITION  -------------------------------------------------------------
-// calculate the position of the q-gram passed as input, in the q-gram profile    
+// calculate the position of the q-gram passed as input, in the q-gram profile
 
 unsigned long long int qposition (int vec[],int size_vec){
 	unsigned long long int pos=0;
@@ -369,7 +329,7 @@ unsigned long long int qposition (int vec[],int size_vec){
 	return pos;
 }
 //////////////////////  PROFILE POSITION => Q-GRAM  -------------------------------------------------------------
-// calculate the q-gram given the position of the q-gram in the profile    
+// calculate the q-gram given the position of the q-gram in the profile
 
 
 void qgramFromPosition (int vec[], unsigned long long int pos){
@@ -380,7 +340,7 @@ void qgramFromPosition (int vec[], unsigned long long int pos){
 }
 
 //////////////////////  MYPOW  -------------------------------------------------------------
-// calculate the power a^b, and then saves the result in a long long int   
+// calculate the power a^b, and then saves the result in a long long int
 
 
 unsigned long long int myPow(int a, int b){
